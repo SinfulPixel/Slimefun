b31f26894e9f5dbf3fb3df4ca19ce7c8311ef7c0
mrCookieSlime <mrCookieSlime@gmail.com>
2014-02-26 19:36:28 +0100

    Fixed Bug that the folder's contents don't get cleared

--- src/me/mrCookieSlime/Slimefun/Setup/Folder.java
+++ src/me/mrCookieSlime/Slimefun/Setup/Folder.java
@@ -8,18 +8,38 @@ public class Folder {
 		File trash = new File("plugins/Slimefun/supported-plugins");
 		if (trash.exists()) {
 			File cleaned = new File("plugins/Slimefun");
-			cleaned.delete();
-			System.out.println("#########################################");
-			System.out.println("");
+			System.out.println("################################################");
+			System.out.println("################################################");
+			System.out.println("################################################");
+			System.out.println("      ");
 			System.out.println("Slimefun Warning:");
-			System.out.println("");
+			System.out.println("         ");
 			System.out.println("Slimefun has detected that your Files are too old and no longer work so it deleted your old Slimefun Folder and generated a new one. Your previous Settings are lost though, but at least Slimefun has saved you from a fatal Slimefun Error which would mess everything up and propably would have make Slimefun no longer work!");
-			System.out.println("");
-			System.out.println("#########################################");
+			System.out.println("        ");
+			System.out.println("################################################");
+			System.out.println("################################################");
+			System.out.println("################################################");
+			
+			delete(cleaned);
 		}
 		else {
 			System.out.println("[Slimefun] All Files are up to date and will propably not cause any Errors");
 		}
+	}
+	
+	public static void delete(File folder) {
+		File[] files = folder.listFiles();
+		if (files != null) {
+			for (File current: files) {
+				if (current.isDirectory()) {
+					delete(current);
+				}
+				else {
+					current.delete();
+				}
+			}
+		}
+		folder.delete();
 	}
 
 }
