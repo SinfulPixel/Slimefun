1367cd2f181a7b143a3db1710fc5cc8735e54a5b
mrCookieSlime <mrCookieSlime@gmail.com>
2014-03-02 20:43:49 +0100

    Removed /milk

--- src/me/mrCookieSlime/Slimefun/Setup/Commands.java
+++ src/me/mrCookieSlime/Slimefun/Setup/Commands.java
@@ -3,7 +3,6 @@ package me.mrCookieSlime.Slimefun.Setup;
 import me.mrCookieSlime.Slimefun.startup;
 import me.mrCookieSlime.Slimefun.Commands.SlimefunCommand;
 import me.mrCookieSlime.Slimefun.Commands.SlimefunTabCompleter;
-import me.mrCookieSlime.Slimefun.Commands.commands;
 
 public class Commands {
 	
@@ -13,13 +12,8 @@ public class Commands {
 		
 		plugin = instance;
 		
-		commands cmd = new commands(plugin);
 		SlimefunCommand cmdi = new SlimefunCommand(plugin);
 		SlimefunTabCompleter tab = new SlimefunTabCompleter();
-		
-		if (plugin.getConfig().getBoolean("disable-features-except-the-items") == false) {
-			plugin.getCommand("milk").setExecutor(cmd);
-		}
 		
 		plugin.getCommand("slimefun").setTabCompleter(tab);
 		plugin.getCommand("sf").setTabCompleter(tab);
