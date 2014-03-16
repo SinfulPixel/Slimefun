8b9f9ce133568f7d413e045d30f5426b150b238b
mrCookieSlime <mrCookieSlime@gmail.com>
2014-03-16 20:42:42 +0100

    Cleanup of old Code

--- src/me/mrCookieSlime/Slimefun/Dictionary/VillagerDictionary.java
+++ src/me/mrCookieSlime/Slimefun/Dictionary/VillagerDictionary.java
@@ -1,12 +1,10 @@
 package me.mrCookieSlime.Slimefun.Dictionary;
 
-import java.util.ArrayList;
-import java.util.List;
+import me.mrCookieSlime.Slimefun.Items.ItemModifier;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class VillagerDictionary {
 	
@@ -15,39 +13,19 @@ public class VillagerDictionary {
 		
 		if (ConfigName.equalsIgnoreCase("Merchant")) {
 			item.setType(Material.MONSTER_EGG);
-			item = Setname(item, ChatColor.RESET + "Spawn \"" + ChatColor.GRAY + "Lost " + ChatColor.BLUE + "Merchant" + ChatColor.RESET + "\"");
-			item = setDurability(item, 120);
+			item = ItemModifier.Setname(item, ChatColor.RESET + "Spawn \"" + ChatColor.GRAY + "Lost " + ChatColor.BLUE + "Merchant" + ChatColor.RESET + "\"");
+			item = ItemModifier.setDurability(item, 120);
 		}
 		if (ConfigName.equalsIgnoreCase("quest")) {
 			item.setType(Material.MONSTER_EGG);
-			item = Setname(item, ChatColor.RESET + "Spawn \"" + ChatColor.GREEN + "Quest " + ChatColor.BLUE + "Villager" + ChatColor.RESET + "\"");
-			item = setDurability(item, 120);
+			item = ItemModifier.Setname(item, ChatColor.RESET + "Spawn \"" + ChatColor.GREEN + "Quest " + ChatColor.BLUE + "Villager" + ChatColor.RESET + "\"");
+			item = ItemModifier.setDurability(item, 120);
 		}
 		if (ConfigName.equalsIgnoreCase("blacksmith")) {
 			item.setType(Material.MONSTER_EGG);
-			item = Setname(item, ChatColor.RESET + "Spawn \"" + ChatColor.GRAY + "Lost " + ChatColor.DARK_GRAY + "Blacksmith" + ChatColor.RESET + "\"");
-			item = setDurability(item, 120);
+			item = ItemModifier.Setname(item, ChatColor.RESET + "Spawn \"" + ChatColor.GRAY + "Lost " + ChatColor.DARK_GRAY + "Blacksmith" + ChatColor.RESET + "\"");
+			item = ItemModifier.setDurability(item, 120);
 		}
 		return item;
 	}
-	
-	public static ItemStack Setname(ItemStack item, String Name) {
-   	    ItemMeta im = item.getItemMeta();
-   	    im.setDisplayName(Name);
-   	    item.setItemMeta(im);
-   	    return item;
-   	  }
-		public static ItemStack setLore(ItemStack item, String lore){
-         ItemMeta im = item.getItemMeta();
-         List<String> ll = new ArrayList<String>();
-         ll.add(lore);
-         im.setLore(ll);
-         item.setItemMeta(im);
-         return item;
-       }
-		public static ItemStack setDurability(ItemStack item, int i) {
-			item.setDurability((short) i);
-			return item;
-		}
-
 }
